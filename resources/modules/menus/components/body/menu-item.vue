70cfd86166764a0e7f88e62cf134f47ca6a49977
Jean Da Rocha <jean.darocha@protonmail.com>
2025-03-24 09:17:13 +0100

    fix(navigation): fix sidebar navigation issues with router link

--- resources/modules/menus/components/body/menu-item.vue
+++ resources/modules/menus/components/body/menu-item.vue
@@ -1,21 +1,32 @@
 <script setup lang="ts">
 import type { NavigationType } from '@/modules/menus/types/navigation-type';
+import type { NavigationResponse } from 'hybridly';
 
-defineProps<{ menuItem: NavigationType }>();
+const props = defineProps<{ menuItem: NavigationType }>();
 
 const { current } = useRoute();
+
+function handleNavigation(): Promise<NavigationResponse> {
+  if (!props.menuItem.routeName) {
+    return router.reload();
+  }
+
+  return router.get(route(props.menuItem.routeName));
+}
 </script>
 
 <template>
   <li>
     <RouterLink
-      href="#"
-      class="relative flex flex-col items-center justify-center w-16 h-16 hover:text-primary-600 dark:hover:text-primary-300"
+      class="relative flex flex-col items-center justify-center w-16 h-16 hover:text-primary-600 dark:hover:text-primary-300 cursor-pointer"
       :class="[
         (menuItem.routeName && menuItem.routeName === current)
           ? 'text-primary dark:text-primary-400'
           : 'dark:text-surface-0',
       ]"
+      method="get"
+      as="button"
+      @click.prevent="handleNavigation"
     >
       <span class="pi !text-xl" :class="menuItem.icon" />
       <span class="text-xs mt-3 text-center break-words px-2">{{ menuItem.navigationLinkName }}</span>
