2ce0f7a14e710e8a011f5578a2c938f586e1971a
Jean Da Rocha <jean.darocha@protonmail.com>
2025-04-22 22:30:29 +0200

    chore(navigation): minor change to navigation link texts

--- resources/modules/shared/routes/navigation.ts
+++ resources/modules/shared/routes/navigation.ts
@@ -2,7 +2,7 @@ import type { NavigationType } from '@/modules/menus/types/navigation-type';
 
 export const navigation: NavigationType[] = [
   { icon: 'pi-calendar', navigationLinkName: 'Calendar' },
-  { icon: 'pi-search', navigationLinkName: 'Search' },
-  { icon: 'pi-users', navigationLinkName: 'Team', routeName: 'users.index' },
+  { icon: 'pi-folder', navigationLinkName: 'My Files' },
+  { icon: 'pi-users', navigationLinkName: 'Users', routeName: 'users.index' },
   { icon: 'pi-cog', navigationLinkName: 'Settings' },
 ];
