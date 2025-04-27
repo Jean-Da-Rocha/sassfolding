bd31b4f202dc496b5bf27efd53d70a946109d3f2
Jean Da Rocha <jean.darocha@protonmail.com>
2025-04-27 20:02:02 +0200

    chore(message): add missing severity to message component

--- app/Enums/FlashMessage.php
+++ app/Enums/FlashMessage.php
@@ -9,8 +9,10 @@ use Spatie\TypeScriptTransformer\Attributes\TypeScript;
 #[TypeScript]
 enum FlashMessage: string
 {
+    case Contrast = 'contrast';
     case Error = 'error';
     case Info = 'info';
+    case Secondary = 'secondary';
     case Success = 'success';
     case Warning = 'warn';
 }
