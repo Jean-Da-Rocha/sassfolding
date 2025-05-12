28db0d8572237d7ad1bd92ff33d21ed6d2d7ea91
Jean Da Rocha <jean.darocha@protonmail.com>
2025-05-12 09:12:25 +0200

    fix(install): fix macOS install error with sed command

--- make/environment.mk
+++ make/environment.mk
@@ -1,9 +1,15 @@
+ifeq ($(UNIX_SHELL_NAME),Darwin)
+	SED_IN_PLACE = sed -i ''
+else
+	SED_IN_PLACE = sed -i
+endif
+
 .PHONY: configure-husky-hooks
 configure-husky-hooks: ## Dynamically bind Husky hooks to the right Docker container.
 	@echo "$(CYAN)[INFO]: Configuring Husky hooks...$(RESET)"
-	@sed -i'' -E "s|[a-zA-Z0-9_-]+-hybridly|$(PROJECT_NAME_SLUG)-hybridly|g" ./.husky/commit-msg
-	@sed -i'' -E "s|[a-zA-Z0-9_-]+-hybridly|$(PROJECT_NAME_SLUG)-hybridly|g" ./.husky/pre-commit
-	@sed -i'' -E "s|[a-zA-Z0-9_-]+-hybridly|$(PROJECT_NAME_SLUG)-hybridly|g" ./.husky/pre-push
+	@$(SED_IN_PLACE) -E "s|[a-zA-Z0-9_-]+-hybridly|$(PROJECT_NAME_SLUG)-hybridly|g" ./.husky/commit-msg
+	@$(SED_IN_PLACE) -E "s|[a-zA-Z0-9_-]+-hybridly|$(PROJECT_NAME_SLUG)-hybridly|g" ./.husky/pre-commit
+	@$(SED_IN_PLACE) -E "s|[a-zA-Z0-9_-]+-hybridly|$(PROJECT_NAME_SLUG)-hybridly|g" ./.husky/pre-push
 	@echo "$(GREEN)[SUCCESS]: Husky hooks ready.$(RESET)"
 
 .PHONY: setup-local-environment
@@ -11,8 +17,8 @@ setup-local-environment: ## Set up the local environment from the .env.example f
 	@echo "$(CYAN)[INFO]: Setting up the local environment...$(RESET)"
 	@if [ ! -f .env ]; then cp .env.example .env; fi
 	@COMPOSE_PROJECT_NAME=$(PROJECT_NAME_SLUG) envsubst < .env.example > .env
-	@sed -i "s|^COMPOSE_PROJECT_NAME=.*|COMPOSE_PROJECT_NAME=$(PROJECT_NAME_SLUG)|" .env
-	@sed -i "s|^DNSMASQ_FORWARD_PORT=.*|DNSMASQ_FORWARD_PORT=$(DNSMASQ_FORWARD_PORT)|" .env
+	@$(SED_IN_PLACE) "s|^COMPOSE_PROJECT_NAME=.*|COMPOSE_PROJECT_NAME=$(PROJECT_NAME_SLUG)|" .env
+	@$(SED_IN_PLACE) "s|^DNSMASQ_FORWARD_PORT=.*|DNSMASQ_FORWARD_PORT=$(DNSMASQ_FORWARD_PORT)|" .env
 	@echo "$(GREEN)[SUCCESS]: local environment ready.$(RESET)"
 
 .PHONY: setup-testing-environment
