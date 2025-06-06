786dc904137b80afc7f4f7ebf5f18e8a6abf68c2
Jean Da Rocha <jean.darocha@protonmail.com>
2025-06-06 10:31:35 +0200

    fix(eslint): fix eslint errors

--- resources/modules/tables/components/body/body-cell.vue
+++ resources/modules/tables/components/body/body-cell.vue
@@ -2,10 +2,9 @@
   <td
     v-if="$slots.default"
     class="
-      !lg:pl-4
       block border-b border-surface-300 p-2 text-right text-sm
       sm:pl-6
-      lg:table-cell lg:border-b-0 lg:text-left
+      lg:table-cell lg:border-b-0 lg:pl-4 lg:text-left
       dark:border-surface-700
     "
     role="cell"
