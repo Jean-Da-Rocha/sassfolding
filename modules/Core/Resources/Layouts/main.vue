556cf2c00b63ee5d20c64cd5530e1a234fe8f784
Jean Da Rocha <jean.darocha@protonmail.com>
2026-01-24 16:44:54 +0100

    chore(sidebar): replace custom sidebar by nuxt-ui sidebar, wip

--- modules/Core/Resources/Layouts/main.vue
+++ modules/Core/Resources/Layouts/main.vue
@@ -1,44 +1,81 @@
 <script setup lang="ts">
-useToggle(useDark());
+import type { NavigationMenuItem } from '@nuxt/ui';
 
-const isSidebarOpen = ref<boolean>(false);
+const items: NavigationMenuItem[] = [
+  {
+    active: true,
+    icon: 'i-lucide-house',
+    label: 'Home',
+  },
+  {
+    icon: 'i-lucide-inbox',
+    label: 'Inbox',
+  },
+  {
+    icon: 'i-lucide-users',
+    label: 'Contacts',
+  },
+  {
+    children: [
+      {
+        label: 'General',
+      },
+      {
+        label: 'Members',
+      },
+      {
+        label: 'Notifications',
+      },
+    ],
+    defaultOpen: true,
+    icon: 'i-lucide-settings',
+    label: 'Settings',
+  },
+];
 </script>
 
 <template>
   <UApp>
-    <div class="flex h-screen">
-      <Sidebar
-        class="
-          fixed inset-y-0 z-30 flex transform flex-col transition-transform
-          duration-300 ease-in-out
-          lg:translate-x-0
-        "
-        :class="[isSidebarOpen ? 'translate-x-0' : '-translate-x-full']"
-      />
+    <UDashboardGroup>
+      <UDashboardSidebar
+        open
+        :toggle="{
+          color: 'primary',
+          variant: 'subtle',
+          class: 'rounded-full',
+        }"
+      >
+        <template #header>
+          <Logo class="h-5 w-auto" />
+        </template>
 
-      <div
-        v-if="isSidebarOpen"
-        class="
-          fixed inset-0 z-20 bg-black/50 transition-opacity
-          lg:hidden
-        "
-        @click="isSidebarOpen = false"
-      />
+        <UNavigationMenu
+          :items="items"
+          orientation="vertical"
+        />
 
-      <div class="flex flex-1 flex-col">
-        <Navbar @toggle-sidebar="isSidebarOpen = !isSidebarOpen" />
+        <template #footer="{ collapsed }">
+          <UButton
+            :avatar="{
+              src: 'https://github.com/benjamincanac.png',
+            }"
+            :block="collapsed"
+            class="w-full"
+            color="neutral"
+            :label="collapsed ? undefined : 'Benjamin'"
+            variant="ghost"
+          />
+        </template>
+      </UDashboardSidebar>
 
-        <main
-          class="
-            flex-1 px-8 py-10
-            sm:px-6
-            lg:pl-28
-          "
-        >
+      <UDashboardPanel>
+        <UDashboardSidebarToggle />
+
+        <main class="p-8">
           <AlertMessage />
           <slot />
         </main>
-      </div>
-    </div>
+      </UDashboardPanel>
+    </UDashboardGroup>
   </UApp>
 </template>
