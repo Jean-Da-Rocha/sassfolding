1154213efa6cd2682e2175b81db1f895bd286575
Jean Da Rocha <jean.darocha@protonmail.com>
2026-02-12 21:28:37 +0100

    chore(images): remove useless logo images

--- modules/Core/Resources/Application/root.blade.php
+++ modules/Core/Resources/Application/root.blade.php
@@ -6,14 +6,6 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <link href="{{ asset('apple-touch-icon.png') }}" rel="apple-touch-icon">
-    <link href="{{ asset('favicon-16x16.png') }}" rel="icon" sizes="16x16" type="image/png">
-    <link href="{{ asset('favicon-32x32.png') }}" rel="icon" sizes="32x32" type="image/png">
-    <link href="{{ asset('android-chrome-192x192.png') }}" rel="icon" sizes="192x192" type="image/png">
-    <link href="{{ asset('android-chrome-512x512.png') }}" rel="icon" sizes="512x512" type="image/png">
-    <link href="{{ asset('favicon.ico') }}" rel="icon">
-    <link href="{{ asset('site.webmanifest') }}" rel="manifest">
-
     @vite
 </head>
 <body class="font-sans antialiased h-full isolate">
